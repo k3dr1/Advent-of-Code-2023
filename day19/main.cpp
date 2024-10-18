89ee9d75db71674e686afc75e34bf8c9aeb05440
Ked <balkangrid@gmail.com>
2024-10-19 04:40:10 +0800

    Progress on day19p2

--- day19/main.cpp
+++ day19/main.cpp
@@ -20,6 +20,10 @@ struct Part {
     unsigned long long int s;
 };
 
+unsigned long long partSum(const Part &part) {
+    return (part.x + part.m + part.a + part.s);
+}
+
 struct IntervalPart {
     std::pair<unsigned long long, unsigned long long> x{0, maxULL};
     std::pair<unsigned long long, unsigned long long> m{0, maxULL};
@@ -28,14 +32,14 @@ struct IntervalPart {
 };
 
 enum class Property {
-    x = 0,
-    m = 1,
-    a = 2,
-    s = 3
+    x = 'x',
+    m = 'm',
+    a = 'a',
+    s = 's'
 };
 
 struct ConditionalRule {
-    Property p;
+    Property property;
     std::pair<unsigned long long, unsigned long long> interval;
     std::string dest;
 };
@@ -46,6 +50,156 @@ struct UnconditionalRule {
 
 using Rule = std::variant<ConditionalRule, UnconditionalRule>;
 
+bool inInterval(const auto &x, const auto &interval) {
+    return (interval.first <= x) && (x <= interval.second);
+}
+
+bool isConditional(Rule rule) {
+    return rule.index() == 0;
+}
+
+std::pair<unsigned long long, unsigned long long> invertRuleInterval(std::pair<unsigned long long, unsigned long long> interval)
+{
+    if (interval.first == 1) {
+        return { interval.second + 1, maxULL };
+    } else {
+        return { 1, interval.first - 1 };
+    }
+}
+
+bool checkRule(Part part, Rule rule)
+{
+    switch (rule.index()) {
+        case 0: {
+            const auto cRule = std::get<ConditionalRule>(rule);
+            switch (cRule.property) {
+                case Property::x: return inInterval(part.x, cRule.interval);
+                case Property::m: return inInterval(part.m, cRule.interval);
+                case Property::a: return inInterval(part.a, cRule.interval);
+                case Property::s: return inInterval(part.s, cRule.interval);
+            }
+        }
+        case 1: {
+            return true;
+        }
+        default: {
+            std::cout << "checkRule default hit\n";
+            return false;
+        }
+    }
+}
+
+auto& getProperty(IntervalPart &part, Property property) {
+    switch (property) {
+        case (Property::x): return part.x;
+        case (Property::m): return part.m;
+        case (Property::a): return part.a;
+        case (Property::s): return part.s;
+    }
+}
+
+std::pair<unsigned long long, unsigned long long> intervalIntersection(
+    std::pair<unsigned long long, unsigned long long> a,
+    std::pair<unsigned long long, unsigned long long> b)
+{
+    auto smallerTop = std::min(a.second, b.second);
+    auto largerBot = std::max(a.first, b.first);
+    if (largerBot <= smallerTop) {
+        return {largerBot, smallerTop};
+    } else {
+        return {0,0};
+    }
+}
+
+std::vector<IntervalPart> construct(
+    IntervalPart intervalPart,
+    const std::string &name,
+    int ruleIdx,
+    const std::unordered_map<std::string, std::vector<Rule>>& workflows
+)
+{
+    if (name == "A") return {intervalPart};
+    if (name == "R") return {};
+
+    const auto workflow = workflows.at(name);
+    const auto rule = workflow.at(ruleIdx);
+
+    if (!isConditional(rule)) {
+        if (std::get<UnconditionalRule>(rule).dest == "A") {
+            return {intervalPart};
+        } else {
+            return {};
+        }
+    }
+
+    const auto conditionalRule = std::get<ConditionalRule>(rule);
+    const auto ruleInterval = conditionalRule.interval;
+
+    std::cout << "rule interval: [" << ruleInterval.first << "," << ruleInterval.second << "]\n";
+
+    const auto acceptedInterval{intervalIntersection(
+        getProperty(intervalPart, conditionalRule.property),
+        conditionalRule.interval
+    )};
+    auto acceptedIntervalPart = IntervalPart{intervalPart};
+    getProperty(acceptedIntervalPart, conditionalRule.property).first = acceptedInterval.first;
+    getProperty(acceptedIntervalPart, conditionalRule.property).second = acceptedInterval.second;
+
+    const auto rejectedInterval{intervalIntersection(
+        getProperty(intervalPart, conditionalRule.property),
+        invertRuleInterval(conditionalRule.interval)
+    )};
+    auto rejectedIntervalPart = IntervalPart{intervalPart};
+    getProperty(rejectedIntervalPart, conditionalRule.property).first = rejectedInterval.first;
+    getProperty(rejectedIntervalPart, conditionalRule.property).second = rejectedInterval.second;
+
+    std::vector<IntervalPart> res{};
+    if (acceptedInterval != std::make_pair(0ULL,0ULL)) {
+        const auto acceptedResults = construct(acceptedIntervalPart,
+            conditionalRule.dest,
+            0,
+            workflows);
+        for (const auto acceptedResult : acceptedResults) {
+            res.push_back(acceptedResult);
+        }
+    }
+    if (rejectedInterval != std::make_pair(0ULL, 0ULL)) {
+        const auto rejectedResults = construct(rejectedIntervalPart, name, ruleIdx+1, workflows);
+        for (const auto rejectedResult : rejectedResults) {
+            res.push_back(rejectedResult);
+        }
+    }
+
+    return res;
+}
+
+bool trace(
+    Part part,
+    std::string &firstWorkflow,
+    std::unordered_map<std::string, std::vector<Rule>> &workflows)
+{
+    auto workflow = workflows.at(firstWorkflow);
+    auto i = 0;
+    while (true) {
+        auto rule = workflow.at(i);
+        if (checkRule(part, rule)) {
+            i = 0;
+            const auto dest = std::visit([](const auto &r){ return r.dest; }, rule);
+            if (dest == "A" || dest == "R") {
+                return dest=="A";
+            }
+            else {
+
+                //std::cout << "dest " << dest << '\n';
+
+                workflow = workflows.at(dest);
+            }
+        } else {
+            i++;
+        }
+    }
+}
+
 int main()
 {
     auto f = std::ifstream { "example.txt" };
@@ -55,36 +209,53 @@ int main()
     std::string line;
     while (std::getline(f, line)) {
         if (line.at(0) != '{') {
+            // skip if empty line
+            if (line.length() <= 1) continue;
+
             // Workflows
             std::vector<Rule> workflow;
-            auto i = 0;
-            while (line.at(i) != '{') i++;
-            const auto name = line.substr(0, i);
-            i++;
+            const auto name = line.substr(0, line.find('{'));
+            std::cout << "Parsing workflow: " << name << '\n';
+            std::cout << "workflow name length: " << name.length() << '\n';
 
             auto split = line 
                 | std::views::drop_while([](const auto c){ return c != '{'; }) 
                 | std::views::drop(1)
                 | std::views::take_while([](const auto c){ return c != '}'; })
                 | std::views::split(',')
-                | std::views::transform([](const auto&& s){return std::string_view(&*s.begin(), std::ranges::distance(s));})
-            ;
+                | std::views::transform([](const auto&& s){return std::string_view(&*s.begin(), std::ranges::distance(s));});
+
             for (auto const &r : split) {
                 if (r.contains(':')) {
                     // Conditional rule
-                    const auto p = Property{r.at(0)};
-                    const auto sign = r.at(1);
-                    int num = 0;
-                    std::from_chars(r.data(), r.data() + r.size() - 1, &num);
-                    const auto interval = (r.at(1) == '<') 
-                        ? std::make_pair(0, std::stoll(std::string(r), 2)) : {};
-                    workflow.emplace_back(ConditionalRule(p, ))
+                    const auto p = Property { r.at(0) };
+                    auto num = 0ULL;
+                    std::from_chars(r.data() + 2, r.data() + r.size(), num, 10);
+
+                    workflow.emplace_back(
+                        ConditionalRule(
+                            p,
+                            (r.at(1) == '<') ? std::make_pair(1ULL, num-1) : std::make_pair(num+1, maxULL),
+                            std::string { r.substr(r.find(':')+1) }));
+
+                    auto back = std::get<ConditionalRule>(workflow.at(workflow.size() - 1));
+
+                    //std::cout << "Conditional rule: property=" << static_cast<char>(back.property)
+                    //    << " interval=[" << back.interval.first 
+                    //    << "," << back.interval.second
+                    //    << "] dest=" << back.dest 
+                    //    << '\n';
+
                 } else {
                     // Unconditional rule
+
+                    //std::cout << "Unconditional rule: dest=" << std::string(r) << '\n';
+                    
                     workflow.emplace_back(UnconditionalRule{std::string(r)});
                 }
             }
-
+            std::cout << "Adding the workflow: " << name << "\n\n";
+            workflows[name] = workflow;
         } else {
             // Parts
             std::vector<unsigned long long> properties {};
@@ -102,6 +273,57 @@ int main()
                 properties.at(3));
         }
     }
+
+    auto part1 = 0;
+    auto part2 = 0;
+    auto count = 0;
+    std::string start = "in";
+    //for (const auto &part : parts) {
+    //    count++;
+    //    std::cout << "tracing part_" << count << " - ";
+    //    auto status = trace(part, start, workflows);
+    //    std::cout << status << '\n';
+    //    if (status) part1 += partSum(part);
+    //}
+
+    //std::cout << "Answer for part 1 is " << part1 << '\n';
+
+    std::cout << "Starting part 2\n";
+
+    auto intervalPart = IntervalPart(
+        {1ULL, 4000ULL},
+        {1ULL, 4000ULL},
+        {1ULL, 4000ULL},
+        {1ULL, 4000ULL}
+    );
+
+    const auto generatedParts = 
+        construct(intervalPart, "in", 0, workflows);
+
+    for (const auto part : generatedParts) {
+        std::cout << "x " << part.x.first << " to " << part.x.second << '\n';
+        std::cout << "m " << part.m.first << " to " << part.m.second << '\n';
+        std::cout << "a " << part.a.first << " to " << part.a.second << '\n';
+        std::cout << "s " << part.s.first << " to " << part.s.second << "\n\n";
+
+        unsigned long long X = 0;
+        unsigned long long M = 0;
+        unsigned long long A = 0;
+        unsigned long long S = 0;
+        for (int i = part.x.first; i <= part.x.second; i++) X += i;
+        for (int i = part.m.first; i <= part.m.second; i++) X += i;
+        for (int i = part.a.first; i <= part.a.second; i++) X += i;
+        for (int i = part.s.first; i <= part.s.second; i++) X += i;
+
+        unsigned long long x_bar = part.x.second - part.x.first + 1;
+        unsigned long long m_bar = part.m.second - part.m.first + 1;
+        unsigned long long a_bar = part.a.second - part.a.first + 1;
+        unsigned long long s_bar = part.s.second - part.s.first + 1;
+
+        part2 += X + x_bar*M + x_bar*m_bar*A + x_bar*m_bar*a_bar*S;
+    }
+
+    std::cout << "Answer for part 2 is " << part2 << '\n';
 
     return 0;
 }
